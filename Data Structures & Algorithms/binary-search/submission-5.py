fab73cf3a6ccd611325fc97468167cea2aea7094
Ahmet Karakoc <ahmet_turan_karakoc@outlook.com>
2026-09-12 19:20:20 +0200

    Remove: binary-search - submission-5

--- Data Structures & Algorithms/binary-search/submission-5.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        check = {}
-        for i in range(len(nums)):
-                check[nums[i]] = i
-        if target in check:
-            return check[target]
-        else:
-            return -1
