4b2b60eb169eb7070916e2134809e7ae975bcb88
Ahmet Karakoc <ahmet_turan_karakoc@outlook.com>
2026-09-13 18:00:20 +0200

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # [1,2,3,4] target 9 # k = per hour eating
+
+        l,r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for i in range(len(piles)):
+                hours += math.ceil(piles[i] / k)
+
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+                
+        return res
+
+            
+
+
+
+
+
+
